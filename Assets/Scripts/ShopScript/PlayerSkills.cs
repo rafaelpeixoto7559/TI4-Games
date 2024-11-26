be6101d86dc04f7bb57c4f2223af84f62ccb286d
Tupacao <arthur12268@gmail.com>
2024-11-25 22:50:11 -0300

    Add de itens na loja

--- Assets/Scripts/ShopScript/PlayerSkills.cs
+++ Assets/Scripts/ShopScript/PlayerSkills.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class PlayerSkills : MonoBehaviour
 {
     public bool hasDash = false;        // Habilidade de Dash
-    public bool hasDoubleJump = false;  // Habilidade de pulo duplo
-    public bool hasShield = false;      // Habilidade de escudo
+    public bool hasHeart = false;  // Habilidade de pulo duplo
+    public bool hasAttack = false;      // Habilidade de escudo
 
     // Você pode adicionar outras habilidades aqui futuramente!
 }
